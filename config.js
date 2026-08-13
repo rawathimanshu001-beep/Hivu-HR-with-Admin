60122307ae840f65132643c03b245cf9aa8ac4d7
rawathimanshu001-beep <rawathimanshu001@gmail.com>
2026-08-13 18:06:46 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-window.HIVU_CONFIG = {
-  supabaseUrl: "https://kaiiffxgnpjsgswuhukj.supabase.co",
-  supabaseKey: "sb_publishable_C-_YpbQRpxvU3l1pcUjIHg_rmDPGRnr",
-  office: {
-    lat: 28.7041,
-    lng: 77.1025,
-    radius: 500
-  }
-};
